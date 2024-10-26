d59e2e0be6c99af4dc553d788e9ffe2545288b9d
Andrew Nelson <nels4766@gmail.com>
2024-10-25 18:00:17 -0600

    User Story 1

--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -7,7 +7,9 @@ public class CashRegister {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		
+		System.out.println("Please enter the price of the item (and be honest!): ");
+			double x = sc.nextDouble();
+			
 		
 	}
 
