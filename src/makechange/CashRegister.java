ce84b5aa68c00d55d6015557904219047b049c61
Andrew Nelson <nels4766@gmail.com>
2024-10-26 12:32:11 -0600

    CashRegister

--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -7,10 +7,23 @@ public class CashRegister {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("Please enter the price of the item (and be honest!): ");
+		System.out.println("Please enter the price of the item: ");
 			double x = sc.nextDouble();
 			
+		System.out.println("Please enter the amount tendered: ");
+			double y = sc.nextDouble();
+			
+		System.out.println("Does not compute. ");
 		
+		System.out.println(result);
+		
+		sc.close();
 	}
-
+	public static double calcChange(double x, double y) {
+		double result = 0.0;
+		for() {
+			
+		}
+	}
+	
 }
