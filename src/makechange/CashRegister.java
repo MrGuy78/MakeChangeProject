df2cf71f0bfd7161b3ab1d05224caeb5d71c185f
Andrew Nelson <nels4766@gmail.com>
2024-10-26 12:42:44 -0600

    CashRegister

--- src/makechange/CashRegister.java
+++ src/makechange/CashRegister.java
@@ -13,17 +13,21 @@ public class CashRegister {
 		System.out.println("Please enter the amount tendered: ");
 			double y = sc.nextDouble();
 			
-		System.out.println("Does not compute. ");
-		
 		System.out.println(result);
 		
 		sc.close();
 	}
 	public static double calcChange(double x, double y) {
 		double result = 0.0;
-		for() {
+		if(x > y) {
+			result = (x - y);
 			
+		else if(x < y) {
+			System.out.println("Does not compute. ");
+				
+			}
 		}
+		return result();
 	}
 	
 }
